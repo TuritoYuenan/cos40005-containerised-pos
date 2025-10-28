74d76883d320a38266093369af2092a575f2cca9
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-10-28 23:31:09 +0700

    chore: update, optimise, and use version catalog for several dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,8 +45,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
-			implementation("io.ktor:ktor-client-okhttp:3.3.1")
-
+			implementation(libs.ktor.client.okhttp)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -58,9 +57,9 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
 			implementation(project.dependencies.platform("io.github.jan-tennert.supabase:bom:3.2.5"))
-			implementation("io.github.jan-tennert.supabase:postgrest-kt")
-			implementation("io.github.jan-tennert.supabase:auth-kt")
-			implementation("io.github.jan-tennert.supabase:realtime-kt")
+			implementation(libs.postgrest.kt)
+			implementation(libs.auth.kt)
+			implementation(libs.realtime.kt)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -68,16 +67,16 @@ kotlin {
         jvmMain.dependencies {
             implementation(compose.desktop.currentOs)
             implementation(libs.kotlinx.coroutinesSwing)
-			implementation("io.ktor:ktor-client-cio:3.3.1")
+			implementation(libs.ktor.client.cio)
         }
 		jsMain {
 			dependencies {
-				implementation("io.ktor:ktor-client-js:3.3.1")
+				implementation(libs.ktor.client.js)
 			}
 		}
 		iosMain {
 			dependencies {
-				implementation("io.ktor:ktor-client-darwin:3.3.1")
+				implementation(libs.ktor.client.darwin)
 			}
 		}
     }
