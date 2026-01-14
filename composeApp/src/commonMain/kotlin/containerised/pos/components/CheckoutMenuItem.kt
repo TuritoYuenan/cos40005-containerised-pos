c21fb154d05fe5ff1365dd7e7f0383f51f86e44f
Quang Nguyen <quangnp2005@gmail.com>
2026-01-14 21:25:01 +0700

    add checkout page

---
+++ composeApp/src/commonMain/kotlin/containerised/pos/components/CheckoutMenuItem.kt
@@ -0,0 +1,108 @@
+package containerised.pos.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun CheckoutMenuItem(name: String, price: Float) {
+	var value by remember { mutableStateOf(1) }
+	Box {
+		Row(
+			modifier = Modifier
+				.fillMaxWidth(),
+			horizontalArrangement = Arrangement.spacedBy(24.dp, alignment = Alignment.CenterHorizontally),
+		) {
+			Box(
+				modifier = Modifier
+					.size(76.dp)
+					.clip(RoundedCornerShape(8.dp))
+					.background(Color(0xFF636262)),
+				contentAlignment = Alignment.Center
+			) {}
+			Column(
+				modifier = Modifier
+					.height(76.dp),
+
+				verticalArrangement = Arrangement.SpaceEvenly
+			) {
+				Text(
+					text = name,
+					style = MaterialTheme.typography.titleSmall
+				)
+				Text(
+					text = "$$price",
+					style = MaterialTheme.typography.bodySmall
+				)
+				Row {
+					Text(
+						text = "Total: $" + value*price,
+					)
+				}
+			}
+			Row(
+				verticalAlignment = Alignment.CenterVertically,
+				horizontalArrangement = Arrangement.spacedBy(12.dp),
+				modifier = Modifier
+					.height(76.dp),
+			){
+				Button(
+					modifier = Modifier.size(32.dp),
+					onClick = { value-- },
+					colors = ButtonDefaults.buttonColors(
+						containerColor = Color(0xFF2196F3),
+						contentColor = Color.Black
+					),
+					shape = RoundedCornerShape(4.dp),
+					contentPadding = PaddingValues(0.dp)
+				) {
+					Icon(
+						Icons.Filled.Remove,
+						contentDescription = "Decrease"
+					)
+				}
+				Text(
+					text = value.toString(),
+				)
+				Button(
+					modifier = Modifier
+						.size(32.dp),
+					onClick = { value++ },
+					colors = ButtonDefaults.buttonColors(
+						containerColor = Color(0xFF2196F3),
+						contentColor = Color.Black
+					),
+					shape = androidx.compose.foundation.shape.RoundedCornerShape(4.dp),
+					contentPadding = PaddingValues(0.dp)
+				){
+					Icon(Icons.Filled.Add, contentDescription = "Decrease")
+				}
+			}
+			Button(
+				modifier = Modifier
+					.size(24.dp),
+				onClick = {},
+				colors = ButtonDefaults.buttonColors(
+					containerColor = Color(0x00FFFFFF),
+					contentColor = Color.Black
+				),
+				contentPadding = PaddingValues(0.dp)
+			){
+				Icon(Icons.Filled.Edit, contentDescription = "Edit")
+			}
+		}
+	}
+}
---
+++ composeApp/src/commonMain/kotlin/containerised/pos/views/CheckoutWebPage.kt
@@ -0,0 +1,128 @@
+package containerised.pos.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Discount
+import androidx.compose.material.icons.filled.RoomService
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import containerised.pos.components.CheckoutDiscountItem
+import containerised.pos.components.CheckoutMenuItem
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun CheckOutWebPage() {
+	LazyColumn(
+		modifier = Modifier
+			.background(Color.White)
+	) {
+		item {
+			Box(
+				modifier = Modifier
+					.fillMaxWidth()
+					.height(40.dp)
+					.background(Color.White),
+				contentAlignment = Alignment.Center
+			) {
+				Text(
+					text = "My Cart",
+					style = MaterialTheme.typography.titleMedium,
+					color = Color.Black
+				)
+			}
+			Card(
+				modifier = Modifier
+					.fillMaxWidth()
+					.padding(vertical = 6.dp, horizontal = 12.dp),
+				shape = RoundedCornerShape(12.dp),
+				elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+			) {
+				Column {
+					Row(
+						modifier = Modifier
+							.padding(vertical = 6.dp),
+					) {
+						Icon(
+							Icons.Filled.RoomService,
+							contentDescription = "Decrease"
+						)
+						Text(
+							text = "Table 1's order",
+							style = MaterialTheme.typography.titleMedium,
+						)
+					}
+					Column(
+						modifier = Modifier
+							.fillMaxWidth()
+							.padding(vertical = 3.dp, horizontal = 12.dp),
+						verticalArrangement = Arrangement.spacedBy(6.dp)
+					) {
+						CheckoutMenuItem("Lorem isum", 100f)
+						CheckoutMenuItem("Lorem isum", 100f)
+						CheckoutMenuItem("Lorem isum", 100f)
+					}
+				}
+			}
+			Card(
+				modifier = Modifier
+					.fillMaxWidth()
+					.padding(vertical = 6.dp, horizontal = 12.dp),
+				shape = RoundedCornerShape(12.dp),
+				elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+			) {
+				Column {
+					Row(
+						modifier = Modifier
+							.padding(vertical = 6.dp),
+					) {
+						Icon(
+							Icons.Filled.Discount,
+							contentDescription = "Decrease"
+						)
+						Text(
+							text = "Table 1's discount",
+							style = MaterialTheme.typography.titleMedium,
+						)
+					}
+					Column(
+						modifier = Modifier
+							.fillMaxWidth()
+							.padding(vertical = 3.dp, horizontal = 12.dp),
+						verticalArrangement = Arrangement.spacedBy(6.dp)
+					) {
+						CheckoutDiscountItem()
+						CheckoutDiscountItem()
+					}
+				}
+				Card {
+					Row(
+						modifier = Modifier
+							.fillMaxWidth()
+							.clip(RoundedCornerShape(2.dp))
+							.background(Color(0xFF2196F3))
+							.padding(vertical = 6.dp, horizontal = 6.dp),
+
+						horizontalArrangement = Arrangement.SpaceBetween,
+						verticalAlignment = Alignment.CenterVertically
+					) {
+						Text(
+							text = "Order:"
+						)
+						Text(
+							text = "$300"
+						)
+					}
+				}
+			}
+		}
+	}
+}
