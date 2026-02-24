b9e59792e39a117e64842442a0746c08e2e7478f
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2026-02-24 21:32:25 +0700

    refactor: simplify ImagePicker function signatures

--- composeApp/src/commonMain/kotlin/containerised/pos/ImagePicker.kt
+++ composeApp/src/commonMain/kotlin/containerised/pos/ImagePicker.kt
@@ -1,20 +1,9 @@
 package containerised.pos
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.unit.dp
 
 @Composable
-expect fun rememberImagePickerUri(
-	onResult: (Any?) -> Unit
-): () -> Unit
+expect fun rememberImagePickerUri(onResult: (Any?) -> Unit): () -> Unit
 
 @Composable
-expect fun rememberImagePickerBytes(
-	uri: Any?
-): ByteArray?
+expect fun rememberImagePickerBytes(uri: Any?): ByteArray?
