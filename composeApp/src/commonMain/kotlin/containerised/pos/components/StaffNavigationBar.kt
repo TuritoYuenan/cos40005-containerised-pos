d17fa93b4881299d8013df57cdbaea24c62afdca
Quang Nguyen <quangnp2005@gmail.com>
2026-02-26 11:04:47 +0700

    fix crash

--- composeApp/src/commonMain/kotlin/containerised/pos/components/StaffNavigationBar.kt
+++ composeApp/src/commonMain/kotlin/containerised/pos/components/StaffNavigationBar.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavController
