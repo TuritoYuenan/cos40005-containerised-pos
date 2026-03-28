fb215e369cf6ac5b8a7175efe897e06625917ad3
pquangw <quangnp2005@gmail.com>
2026-03-28 10:26:12 +0700

    WIP

--- composeApp/src/commonMain/kotlin/containerised/pos/views/EmployeeManagementPage.kt
+++ composeApp/src/commonMain/kotlin/containerised/pos/views/EmployeeManagementPage.kt
@@ -3,27 +3,29 @@ package containerised.pos.views
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import containerised.pos.database.SupabaseClient
-import containerised.pos.models.User
+import containerised.pos.models.UserRole
 
 private val defaultPadding = 16.dp
 
 @Composable
 fun EmployeeManagementPage() {
 	val scope = rememberCoroutineScope()
-	var user by remember { mutableStateOf<User?>(null) }
 	val userId = SupabaseClient.auth.currentUserOrNull()?.id
-	val email = SupabaseClient.auth.currentUserOrNull()?.email
+	var userRole by remember { mutableStateOf<UserRole?>(null) }
 
 	LaunchedEffect(Unit) {
 		try {
-			user = User.fetchById(userId ?: "")
+			userRole = UserRole.fetchAndJoin(userId?: "")
 		} catch (e: Exception) {
 			val error = e.message
 			println("Error: $error")
@@ -34,12 +36,48 @@ fun EmployeeManagementPage() {
 		Modifier.padding(defaultPadding, 0.dp),
 		Arrangement.spacedBy(defaultPadding)
 	) {
-		Text("User information", style = MaterialTheme.typography.headlineMedium)
-		Text("Name: ${user?.fullName}", style = MaterialTheme.typography.titleMedium)
-		Text("Email: $email", style = MaterialTheme.typography.titleMedium)
+		userRole?.EmployeeCard()
+		userRole?.DetailCard()
+	}
+}
 
-		Row {
+@Composable
+fun UserRole.EmployeeCard(){
+	Card(Modifier.fillMaxWidth(),) {
+		Column() {
+			Text("${user?.fullName}", style = MaterialTheme.typography.titleLarge)
+		}
+	}
+}
 
+@Composable
+fun UserRole.DetailCard(){
+	Card(Modifier.fillMaxWidth().padding(12.dp, 6.dp),) {
+		Row() {
+			Column() {
+				Text("DEPARTMENT", style = MaterialTheme.typography.bodyMedium)
+				Text(role.roleName, style = MaterialTheme.typography.titleMedium)
+			}
+			Column() {
+				Text("STATUS", style = MaterialTheme.typography.bodyMedium)
+				if (user?.isActive == true) {
+					Text("Active", style = MaterialTheme.typography.titleMedium)
+				} else {
+					Text("Inactive", style = MaterialTheme.typography.titleMedium)
+				}
+			}
+		}
+	}
+	Card(Modifier.fillMaxWidth().padding(12.dp, 6.dp),) {
+		Column() {
+			Text("EMAIL", style = MaterialTheme.typography.bodyMedium)
+			Text(user?.email ?: "null", style = MaterialTheme.typography.bodyMedium)
+			if (user?.phone != null) {
+				Text("PHONE", style = MaterialTheme.typography.bodyMedium)
+				Text(user.phone, style = MaterialTheme.typography.bodyMedium)
+			}
+			Text("JOINED", style = MaterialTheme.typography.bodyMedium)
+			Text(user?.createdAt ?: "null", style = MaterialTheme.typography.bodyMedium)
 		}
 	}
 }
