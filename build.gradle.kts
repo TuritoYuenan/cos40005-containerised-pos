42421d351df5961cfb3debd59dbd8731476b3d2c
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2026-04-06 09:44:10 +0700

    ci: setup SonarQube Cloud integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ sonar {
 	properties {
 		property("sonar.projectKey", "COS40006")
 		property("sonar.projectName", "COS40006 Containerised POS")
+		property("sonar.organization", "containerised")
 		property("sonar.host.url", "http://localhost:9000")
 		property("sonar.coverage.jacoco.xmlReportPaths", "${layout.projectDirectory.file("composeApp/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml").asFile.absolutePath}")
 	}
